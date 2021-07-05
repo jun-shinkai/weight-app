823d472280289d7519584959c610ef5c7331b4a4
jun-shinkai <shinjyuna@gmail.com>
2021-07-05 21:11:24 +0900

    bmi

---
+++ app/javascript/bmi.js
@@ -0,0 +1,15 @@
+if(location.pathname.match("infos/new")){
+  document.addEventListener("DOMContentLoaded",() =>{
+     let h_value = document.getElementById("info_height")
+    h_value.addEventListener("keyup",() =>{
+      let w_value = document.getElementById("info_weight").value
+      let h_value = document.getElementById("info_height").value 
+    h_value /= 100;
+    
+    let bmi = w_value /(h_value * h_value); 
+    bmi = Math.floor(bmi * 10) / 10;
+    const result_Bmi = document.getElementById("bmi-result") 
+    result_Bmi.innerHTML = bmi
+  });
+  });
+};
